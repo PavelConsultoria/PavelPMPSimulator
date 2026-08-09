3aa5670abd2ffce95169a68f1255f6bb6f3ace5f
PavelConsultoria <kmp1313@gmail.com>
2026-08-09 10:08:12 -0300

    Testa rota Banco de Questões

--- src/pages/BancoQuestoes.jsx
+++ src/pages/BancoQuestoes.jsx
@@ -1,3 +1,16 @@
 export default function BancoQuestoes() {
-  return <h1 style={{ color: "red" }}>TESTE BANCO</h1>;
+  return (
+    <div
+      style={{
+        color: "white",
+        backgroundColor: "black",
+        minHeight: "100vh",
+        padding: "50px",
+        textAlign: "center",
+      }}
+    >
+      <h1 style={{ color: "#22c55e" }}>BANCO DE QUESTÕES</h1>
+      <p>Rota funcionando corretamente.</p>
+    </div>
+  );
 }
